80bec962c1f1cb9ab711375f0a1991b07e0d010b
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-02-14 18:19:28 +0200

    add fonts + update normalize

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import {Routes } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import { AppWrapper } from './App.styled';
 
 const test = import.meta.env.VITE_API_TEST;
 
 // const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
 
-
 function App() {
   console.log(test);
   return (
     <AppWrapper>
-      <Routes>
-      </Routes>
+      <Routes></Routes>
     </AppWrapper>
   );
 }
