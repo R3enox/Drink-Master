1ae174d749fbdc72597be9e5728b6637da70414c
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-02-18 22:50:21 +0200

    center

--- src/App.styled.jsx
+++ src/App.styled.jsx
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 import 'normalize.css';
 
 export const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
   width: 100%;
   height: 100vh;
   background-repeat: no-repeat;
