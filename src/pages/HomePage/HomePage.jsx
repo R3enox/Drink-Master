a9b872dd03cd5a5503fd731117a071b5ff17ebd3
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-02-18 19:05:01 +0200

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,64 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Hero } from '../../components/Hero/Hero';
 import { PreviewDrinks } from '../../components/Drinks/PreviewDrinks';
 
-import imageSrc from './img/asr_blue_iced_tea_4203516a_f9a0_4011_b97a_c7044eabc27f 1.png';
-import imageSrc1 from './img/Ellipse 222.png';
-import imageSrc2 from './img/Ellipse.png';
-import imageSrc3 from './img/Ellipse 224.png';
 function HomePage() {
   return (
     <div>
-      <div className="mb-[123px]">
-        <div className="lg:w-[715px] lg:h-[330px] lg:mt-[185px] lg:ml-[100px]">
-          <h1
-            className="title relative font-semibold text-3xl leading-tight  ml-[20px] mt-[152px] mr-[20px] z-10 
-        md:text-[56px] md:font-semibold md:ml-[32px] md:mr-[95px]
-        lg:font-semibold lg:text-[64px] lg:leading-tight lg:m-0"
-          >
-            Craft Your Perfect Drink with Drink Master
-          </h1>
-          <p
-            className="relative font-custom font-normal text-base leading-[143%] text-[14px] ml-5 mr-5 mt-4 z-10
-        md:ml-8 md:mr-[117px] md:mt-7 md:text-[18px] md:text-lg leading-[133%]
-        lg:pt-[28px] lg:text-[18px] lg:leading-[133%] lg:m-0 lg:pr-56 lg:pb-10"
-          >
-            Unlock your inner mixologist with Drink Master, your one-stop
-            destination for exploring, crafting, and mastering the world's
-            finest beverages.
-          </p>
-
-          <Link
-            className="relative rounded-[42px] w-[151px] h-[46px] bg-[#f3f3f3] ml-[20px] mt-[32px] flex justify-center items-center font-semibold text-[16px] leading-[112%] text-[#161f37] z-10
-          md:ml-[32px] md:mt-[48px]
-          lg:font-semibold lg:text-base lg:leading-[112%] lg:rounded-[42px] lg:cc lg:h-[54px] lg:m-0"
-            to="/add-drink"
-          >
-            Add Drink
-          </Link>
-        </div>
-        <img
-          src={imageSrc}
-          alt=""
-          className="relative w-[252px] h-[313px] top-[47px] mb-[112px] ml-[62px] mr-[61px] z-10 
-        md:w-[359px] md:h-[445px] md:ml-[205px] md:mt-[54px]
-        lg:absolute lg:top-[128px] lg:right-[136px] lg:m-0"
-        />
-
-        <img src={imageSrc1} alt="" className="absolute top-0 left-0 z-1" />
-        <img
-          src={imageSrc2}
-          alt=""
-          className="absolute top-[426px] right-[-50px] bottom-0 right-0 z-1
-        md:right-[-20px] md:top-[414px]"
-        />
-
-        <img
-          src={imageSrc3}
-          alt=""
-          className=" hidden lg:block lg:absolute lg:top-[203px] lg:left-[660px] lg:right-[193px] lg:z-1"
-        />
-      </div>
+      <Hero />
       <PreviewDrinks />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ export default {
           "url('./src/assets/img/welcomePage/mobile-bg-welcome-page.png')",
         'mobile-bg-welcome@2x':
           "url('./src/assets/img/welcomePage/mobile-bg-welcome-page@2x.png')",
+        'mobile-bg-footer':
+          "url('./src/assets/img/footer/mobile-bg-footer.png')",
+        'mobile-bg-footer@2x':
+          "url('./src/assets/img/footer/mobile-bg-footer@2x.png')",
+        'tablet-bg-footer':
+          "url('./src/assets/img/footer/tablet-bg-footer.png')",
+        'tablet-bg-footer@2x':
+          "url('./src/assets/img/footer/tablet-bg-footer@2x.png')",
+        'desktop-bg-footer':
+          "url('./src/assets/img/footer/desktop-bg-footer.png')",
+        'desktop-bg-footer@2x':
+          "url('./src/assets/img/footer/desktop-bg-footer@2x.png')",
       },
     },
     container: {
@@ -33,7 +45,9 @@ export default {
       'button-hover-color': '#0A0A11',
       'welcome-text-color': '#FAFAFA',
       'filter-scroll-text': '#434D67',
+      'hover-text-color': '#253d6f',
       'border-color': 'rgba(243, 243, 243, 0.2)',
+      'border-color-add': 'rgba(64, 112, 205, 0.5)',
       'grey-text-color': 'rgba(243, 243, 243, 0.5)',
       transparent: 'rgba(243, 243, 243, 0)',
     },
