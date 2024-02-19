b8de9ee6aad54325bd327d7aff5c5e28a37a4e2c
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-02-19 20:14:51 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { Suspense, useEffect } from 'react';
+import { useEffect, Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { AppWrapper } from './App.styled';
-import WelcomePage from './pages/WelcomePage/WelcomePage';
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
 import HomePage from './pages/HomePage/HomePage';
 import DrinksPage from './pages/DrinksPage/DrinksPage';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import { Suspense, lazy } from 'react';
+import { selectAuthIsRefreshing } from './redux/auth/authSelectors';
+import { refreshUserThunk } from './redux/auth/authOperations';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
 const SignInPage = lazy(() => import('./pages/SignInPage/SignInPage'));
 
 function App() {
-
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectAuthIsRefreshing);
 
@@ -27,21 +25,22 @@ function App() {
 
   return (
     !isRefreshing && (
-    <AppWrapper>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path="/welcome" element={<WelcomePage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<Navigate to="/welcome" />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/drinks" element={<DrinksPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </AppWrapper>
-  ));
+      <AppWrapper>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<Navigate to="/welcome" />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/drinks" element={<DrinksPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </AppWrapper>
+    )
+  );
 }
 export default App;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,7 +1,29 @@
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer/Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFilters } from 'hooks/useFilters';
+import { selectAuthIsLoggedIn } from '../../redux/auth/authSelectors';
+import {
+  getCategories,
+  getGlasses,
+  getIngredients,
+} from '../../redux/filters/operations';
+import { useEffect } from 'react';
 
 const SharedLayout = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectAuthIsLoggedIn);
+  const { categories, ingredients, glasses } = useFilters();
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    if (!categories) dispatch(getCategories());
+    if (!ingredients) dispatch(getIngredients());
+    if (!glasses) dispatch(getGlasses());
+
+    console.log('USE EFFECT');
+  }, [isLoggedIn, categories, ingredients, glasses, dispatch]);
   return (
     <>
       <Outlet />
