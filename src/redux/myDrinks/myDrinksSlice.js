ca4721278316ed1f4e5cca4c39197297bc28a617
Vadym Bobyrenko <wadimcka@gmail.com>
2024-02-21 15:56:06 +0100

    no coment

--- src/redux/myDrinks/myDrinksSlice.js
+++ src/redux/myDrinks/myDrinksSlice.js
@@ -1,32 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// import axios from 'axios';
-
-// const axiosBaseQuery =
-//   ({ baseUrl, token } = { baseUrl: '', token: '' }) =>
-//   async ({ url, method, data, params, headers }) => {
-//     try {
-//       const result = await axios({
-//         url: baseUrl + url,
-//         method,
-//         data,
-//         params,
-//         headers: {
-//           ...headers,
-//           Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZDQ2NzE1MDg4NjllZjczZDJlODVjYyIsImlhdCI6MTcwODUxMTEwNywiZXhwIjoxNzA4NTU0MzA3fQ.0YX9neTmrnaE9Jujqs4lJBJyGkrkT7x-ZrXLOWYrx_k'}`,
-//           //   Authorization: `Bearer ${token}`,
-//         },
-//       });
-//       return { data: result.data };
-//     } catch (axiosError) {
-//       const err = axiosError;
-//       return {
-//         error: {
-//           status: err.response?.status,
-//           data: err.response?.data || err.message,
-//         },
-//       };
-//     }
-//   };
 
 export const myDrinksApi = createApi({
   reducerPath: 'myDrinksApi',
@@ -60,18 +32,3 @@ export const myDrinksApi = createApi({
 });
 
 export const { useFetchMyDrinksQuery, useDeleteMyDrinkMutation } = myDrinksApi;
-
-// export const myDrinksApi = createApi({
-//   reducerPath: 'myDrinksApi',
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: 'https://drink-master-4fm6.onrender.com/api/',
-//   }),
-//   endpoints: (builder) => ({
-//     fetchMyDrinks: builder.query({
-//       query: (id) => `own/${id}`,
-//     }),
-//   }),
-// });
-// console.log(myDrinksApi);
-
-// export const { useFetchMyDrinksQuery } = myDrinksApi;
