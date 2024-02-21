93ab87215c7019a46ef23ea6b7d68ad9e0a0791b
R3enox <129979133+R3enox@users.noreply.github.com>
2024-02-21 09:03:14 +0000

    Deploying to gh-pages from @ R3enox/frontend-drink-master@88aa2cb657b57c57c35cb0b70e79204832e6c793 🚀

---
+++ assets/AddDrinkPage-9de6dbc6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2d4ca33f.js";const r=()=>e.jsx("div",{style:{textAlign:"center"},children:"AddDrinkPage"});export{r as default};
--- assets/MyDrinksPage-3a67bedf.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-bf1cbf5a.js";const r=()=>e.jsx("div",{style:{textAlign:"center"},children:"MyDrinksPage"});export{r as default};
