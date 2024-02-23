24a0e1d8b9da817ff6b576a5757dfcabe075b4b8
Vadym Bobyrenko <wadimcka@gmail.com>
2024-02-23 10:18:14 +0100

    modal position

--- src/components/DrinksItem/UniversalModal.jsx
+++ src/components/DrinksItem/UniversalModal.jsx
@@ -33,7 +33,7 @@ const UniversalModal = ({ children, isOpen, closeFnc, content }) => {
       className="fixed bg-opacity-0 overflow-x-hidden overflow-y-auto left-0 top-0  w-full h-full pt-20 z-100"
       onClick={handleOverlayClick}
     >
-      <div className="absolute bg-primary-text-button-color left-1/2 transform -translate-x-1/2 rounded-[15px] overflow-y-auto shadow-xl p-[50px]">
+      <div className="absolute bg-primary-text-button-color top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded-[24px] items-center flex flex-col p-[50px]">
         <button
           className="absolute items-center flex justify-center top-[18px] right-[18px]"
           onClick={closeOnClick}
@@ -42,7 +42,7 @@ const UniversalModal = ({ children, isOpen, closeFnc, content }) => {
             <use href={sprite + '#icon-cross'}></use>
           </svg>
         </button>
-        <p className="text-[14px] leading-[1.29] md:text-[18px] md:leading-[1.33] mb-[25px] md:mb-[37px]">
+        <p className="text-[14px] leading-[1.29] md:text-[18px] md:leading-[1.33] mb-[25px] md:mb-[37px] ">
           {content}
         </p>
         {children}
