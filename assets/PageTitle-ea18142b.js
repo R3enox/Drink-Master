a5a59b5383fac4b27a862dc002cbedd11e6f2a46
R3enox <129979133+R3enox@users.noreply.github.com>
2024-02-25 18:59:17 +0000

    Deploying to gh-pages from @ R3enox/frontend-drink-master@c4bc6ec63a2c64c66c5a989a250ca7975fb2ad46 🚀

---
+++ assets/PageTitle-ea18142b.js
@@ -0,0 +1,1 @@
+import{j as x}from"./index-53d61fd1.js";const e=({title:t})=>x.jsx("h2",{className:" mb-[40px] font-semibold text-[32px] md:mb-[60px] md:text-[56px] lg:text-[64px] leading-[1.19] md:leading-[1.07] lg:mb-[62px] lg:leading-[1.06] pt-[80px] md:pt-[140px] lg:pt-[160px]  ",children:t});export{e as P};
