f88617dbea170cc7c57fd2eec2e3233c35a8de93
Mykyta Yeremenko <nikr3enox@gmail.com>
2024-02-26 15:11:59 +0200

    first fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,11 @@
 import { Hero } from 'components/Hero/Hero';
 import { PreviewDrinks } from 'components/Drinks/PreviewDrinks';
 
-
 const HomePage = () => {
   return (
-    <div className="bg-contain bg-no-repeat dark:bg-home-set md:dark:bg-home-set-tablet lg:dark:bg-home-set-desktop">
+    <div className="min-w-full bg-contain bg-no-repeat dark:bg-home-set md:dark:bg-home-set-tablet lg:dark:bg-home-set-desktop">
       <Hero />
       <PreviewDrinks />
-
     </div>
   );
 };
