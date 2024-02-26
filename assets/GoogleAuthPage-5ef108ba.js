dcd864c13667dd5c12513ffecea21f0050b1a5c8
R3enox <129979133+R3enox@users.noreply.github.com>
2024-02-26 15:34:32 +0000

    Deploying to gh-pages from @ R3enox/frontend-drink-master@795f719594a8f975c2bbd37c72cbd4f75b70fe11 🚀

---
+++ assets/GoogleAuthPage-5ef108ba.js
@@ -0,0 +1,1 @@
+import{d as o,b as r,I as n,r as h,X as u,Y as f}from"./index-565b7323.js";const m=()=>{const[s,g]=o(),a=s.get("accessToken"),t=s.get("refreshToken"),e=r(),c=n();h.useEffect(()=>{e(u({accessToken:a,refreshToken:t})),e(f()),c("/home")},[a,t,e,c])};export{m as default};
