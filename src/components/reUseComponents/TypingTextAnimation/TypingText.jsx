2fa7cb1c78c5607b20590d2e9ad2422fbf542754
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-02-27 13:05:50 +0100

    update typing text

--- src/components/reUseComponents/TypingTextAnimation/TypingText.jsx
+++ src/components/reUseComponents/TypingTextAnimation/TypingText.jsx
@@ -4,11 +4,13 @@ const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
 import '../../../i18n';
 
 const TypingText = () => {
-  const { t} = useTranslation();
+  const { t } = useTranslation();
+  const currentText = t('TypingText');
   return (
     <TypeAnimation
+      key={currentText}
       sequence={[
-        t('TypingText'),
+        currentText,
         2000,
         (el) => el.classList.remove(CURSOR_CLASS_NAME),
         6000,
