02d3705aab16acd0a6c6b7478d1d09617074e88f
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-02 12:07:44 +0200

    refactor: update MotivatingModal component
    Improve MotivatingModal component to adaptive handle motiv-modal bg classnames

--- src/components/reUseComponents/MotivatingModal/MotivatingModal.jsx
+++ src/components/reUseComponents/MotivatingModal/MotivatingModal.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
+
+import {
+  BgMotivModalClassNames,
+  BgMotivModalKeys,
+} from '../../../constants/motivModalClassNames';
 import sprite from 'assets/sprite.svg';
 
 export const MotivatingModal = ({
   onClose,
   achievementText,
-  mobileBgClassName,
-  tabletDesktopBgClassName,
+  classNamesKey,
 }) => {
   useEffect(() => {
     const handleKeyDown = (e) => {
@@ -27,11 +31,12 @@ export const MotivatingModal = ({
     }
   };
 
+  const mobile = BgMotivModalClassNames[BgMotivModalKeys[classNamesKey]].MOBILE;
+  const tablet = BgMotivModalClassNames[BgMotivModalKeys[classNamesKey]].TABLET;
+
   return (
     <div className="motivating-modal-backdrop" onClick={handleOverlayClick}>
-      <div
-        className={`motivating-modal-modal ${mobileBgClassName} ${tabletDesktopBgClassName}`}
-      >
+      <div className={`motivating-modal-modal ${mobile} ${tablet}`}>
         <p className="motiv-modal-title">{achievementText}</p>
         <button className="motiv-modal-close-btn" onClick={onClose}>
           <svg className="motiv-modal-close-btn-icon">
